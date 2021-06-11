5eb5e889a9c46a8256cf248f5093266537d474fd
Kaiah Miller <65531429+FunkyMcMilker@users.noreply.github.com>
2021-06-11 10:56:22 +0700

    Add files via upload

---
+++ test.php
@@ -0,0 +1,68 @@
+<?php
+  $db_host = 'localhost';
+  $db_user = 'root';
+  $db_password = 'root';
+  $db_db = 'Recording_Studio';
+  $db_port = 8889;
+
+  $mysqli = new mysqli(
+    $db_host,
+    $db_user,
+    $db_password,
+    $db_db
+  );
+	
+  if ($mysqli->connect_error) {
+    echo 'Errno: '.$mysqli->connect_errno;
+    echo '<br>';
+    echo 'Error: '.$mysqli->connect_error;
+    exit();
+  }
+
+  echo 'Success: A proper connection to MySQL was made.';
+  echo '<br>';
+  echo 'Host information: '.$mysqli->host_info;
+  echo '<br>';
+  echo 'Protocol version: '.$mysqli->protocol_version;
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+  
+<div class="container">
+  <h1>My First Bootstrap Page</h1>
+  <p>This part is inside a .container class.</p> 
+  <p>The .container class provides a responsive fixed width container.</p>
+  <form method="post">
+ 	<p>Customer name: <input type="text" name="c_name" /></p>
+ 	<p><input type="submit" /></p>
+  </form>           
+</div>
+
+</body>
+</html>
+
+$sql = "SELECT c_name FROM Customer WHERE c_name='Doe'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "Name: " . $row["c_name"];
+  }
+} else {
+  echo "0 results";
+}
+
+
+  $mysqli->close();
+?>
