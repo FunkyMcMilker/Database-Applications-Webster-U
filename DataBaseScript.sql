fc900d32395573ab9c97a97fff5b99bdbb80f536
Kaiah Miller <65531429+FunkyMcMilker@users.noreply.github.com>
2021-07-16 10:37:15 +0700

    Update DataBaseScript.sql

--- DataBaseScript.sql
+++ DataBaseScript.sql
@@ -3,7 +3,6 @@ CREATE DATABASE myStudio;
 CREATE TABLE Studio_Rooms (
 	studio_id int NOT NULL,
 	studio_rate double,
-	studio_avalibility bit,
 PRIMARY KEY (studio_id)
 );
 
@@ -13,7 +12,6 @@ CREATE TABLE instruments (
 	instrument_name varchar(30),
 	instrument_type varchar(30),
 	instrument_rate double,
-	instrument_avalibility bit,
 PRIMARY KEY (instrument_id)
 );
 
@@ -21,7 +19,6 @@ CREATE TABLE Employees (
 	employee_id int NOT NULL,
 	employee_name varchar(30),
 	possition varchar(30),
-  employee_avalibility bit,
 PRIMARY KEY (employee_id)
 
 );
@@ -47,32 +44,32 @@ CREATE TABLE Customer (
 PRIMARY KEY (customer_id),
 FOREIGN KEY (employee_id) REFERENCES Employees(employee_id),
 FOREIGN KEY (studio_id) REFERENCES Studio_Rooms(studio_id),
-FOREIGN KEY (instrument_id) REFERENCES Instruments(instrument_id)
+FOREIGN KEY (instrument_id) REFERENCES instruments(instrument_id)
 );
 
-INSERT INTO Studio_Rooms (studio_id, studio_rate, studio_avalibility)
-VALUES (1, 50.00, 1);
+INSERT INTO Studio_Rooms (studio_id, studio_rate)
+VALUES (1, 50.00);
 
-INSERT INTO Studio_Rooms (studio_id, studio_rate, studio_avalibility)
-VALUES (2, 75.00, 1);
+INSERT INTO Studio_Rooms (studio_id, studio_rate)
+VALUES (2, 75.00);
 
-INSERT INTO Studio_Rooms (studio_id, studio_rate, studio_avalibility)
-VALUES (3, 100.00, 1);
+INSERT INTO Studio_Rooms (studio_id, studio_rate)
+VALUES (3, 100.00);
 
-INSERT INTO instruments (instrument_id, instrument_name, instrument_type, instrument_rate, instrument_avalibility)
-VALUES (1, "59 Fender Stratocaster", "Guitar", 100.00, 1);
+INSERT INTO instruments (instrument_id, instrument_name, instrument_type, instrument_rate)
+VALUES (1, "59 Fender Stratocaster", "Guitar", 100.00);
 
-INSERT INTO instruments (instrument_id, instrument_name, instrument_type, instrument_rate, instrument_avalibility)
-VALUES (2, "63 Fender P-Bass", "Bass Guitar", 100.00, 1);
+INSERT INTO instruments (instrument_id, instrument_name, instrument_type, instrument_rate)
+VALUES (2, "63 Fender P-Bass", "Bass Guitar", 100.00);
 
-INSERT INTO instruments (instrument_id, instrument_name, instrument_type, instrument_rate, instrument_avalibility)
-VALUES (3, "67 Pearl Drumset", "Drumset", 200.00, 1);
+INSERT INTO instruments (instrument_id, instrument_name, instrument_type, instrument_rate)
+VALUES (3, "67 Pearl Drumset", "Drumset", 200.00);
 
-INSERT INTO Employees (employee_id, equipment_name, possition, employee_avalibility)
-VALUES (1, "Rick Ruben", "Producer", 1);
+INSERT INTO Employees (employee_id, employee_name, possition)
+VALUES (1, "Rick Ruben", "Producer");
 
-INSERT INTO Employees (employee_id, equipment_name, possition, employee_avalibility)
-VALUES (2, "Rapheal Sadiq", "Producer", 2);
+INSERT INTO Employees (employee_id, employee_name, possition)
+VALUES (2, "Rapheal Sadiq", "Producer");
 
 INSERT INTO Equipment (equipment_id, equipment_name, description, studio_id)
 VALUES (1, "Universal Audio 610", "Anolog Console", 1);
@@ -81,13 +78,13 @@ INSERT INTO Equipment (equipment_id, equipment_name, description, studio_id)
 VALUES (2, "Sure Dynamic Mic", "Vocal Microphone", 1);
 
 INSERT INTO Equipment (equipment_id, equipment_name, description, studio_id)
-VALUES (3, "63 Vox AC30", "Guitar Amplifier", 1)
+VALUES (3, "63 Vox AC30", "Guitar Amplifier", 1);
 
-INSERT INTO Customer (customer_id, customer_name, studio_id, employee_avalibility, employee_id, instrument_id )
-VALUES (021, 'Smith', 1, 1, 1, NULL)
+INSERT INTO Customer (customer_id, customer_name, studio_id, employee_id, instrument_id )
+VALUES (021, 'Smith', 1, 1, 3);
 
-INSERT INTO Customer (customer_id, customer_name, studio_id, employee_avalibility, employee_id, instrument_id )
-VALUES (022, 'Dirty Magic', 1, 1, 1, NULL)
+INSERT INTO Customer (customer_id, customer_name, studio_id, employee_id, instrument_id )
+VALUES (022, 'Dirty Magic', 3, 1, 1);
 
-INSERT INTO Customer (customer_id, customer_name, studio_id, employee_avalibility, employee_id, instrument_id )
-VALUES (023, 'Vulfpeck', 1, 1, 1, NULL)
+INSERT INTO Customer (customer_id, customer_name, studio_id, employee_id, instrument_id )
+VALUES (023, 'Vulfpeck', 2, 2, 2);
