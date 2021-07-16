9344b2ec9e8b8457f70c5a42f5266864be6c3e71
Kaiah Miller <65531429+FunkyMcMilker@users.noreply.github.com>
2021-07-16 10:16:25 +0700

    Update DataBaseScript.sql

--- DataBaseScript.sql
+++ DataBaseScript.sql
@@ -19,7 +19,7 @@ PRIMARY KEY (instrument_id)
 
 CREATE TABLE Employees (
 	employee_id int NOT NULL,
-	equipment_name varchar(30),
+	employee_name varchar(30),
 	possition varchar(30),
   employee_avalibility bit,
 PRIMARY KEY (employee_id)
@@ -33,7 +33,7 @@ CREATE TABLE Equipment (
 	description varchar(30),
 	studio_id int,
 PRIMARY KEY (equipment_id),
-FOREIGN KEY (studio_id) REFERENCES studio_Rooms(studio_id)
+FOREIGN KEY (studio_id) REFERENCES Studio_Rooms(studio_id)
 
 );
 
@@ -51,34 +51,34 @@ FOREIGN KEY (instrument_id) REFERENCES Instruments(instrument_id)
 );
 
 INSERT INTO Studio_Rooms (studio_id, studio_rate, studio_avalibility)
-VALUES (1, 50.00, 1)
+VALUES (1, 50.00, 1);
 
 INSERT INTO Studio_Rooms (studio_id, studio_rate, studio_avalibility)
-VALUES (2, 75.00, 1)
+VALUES (2, 75.00, 1);
 
 INSERT INTO Studio_Rooms (studio_id, studio_rate, studio_avalibility)
-VALUES (3, 100.00, 1)
+VALUES (3, 100.00, 1);
 
 INSERT INTO instruments (instrument_id, instrument_name, instrument_type, instrument_rate, instrument_avalibility)
-VALUES (1, "59 Fender Stratocaster", "Guitar", 100.00, 1)
+VALUES (1, "59 Fender Stratocaster", "Guitar", 100.00, 1);
 
 INSERT INTO instruments (instrument_id, instrument_name, instrument_type, instrument_rate, instrument_avalibility)
-VALUES (2, "63 Fender P-Bass", "Bass Guitar", 100.00, 1)
+VALUES (2, "63 Fender P-Bass", "Bass Guitar", 100.00, 1);
 
 INSERT INTO instruments (instrument_id, instrument_name, instrument_type, instrument_rate, instrument_avalibility)
-VALUES (3, "67 Pearl Drumset", "Drumset", 200.00, 1)
+VALUES (3, "67 Pearl Drumset", "Drumset", 200.00, 1);
 
 INSERT INTO Employees (employee_id, equipment_name, possition, employee_avalibility)
-VALUES (1, "Rick Ruben", "Producer", 1)
+VALUES (1, "Rick Ruben", "Producer", 1);
 
 INSERT INTO Employees (employee_id, equipment_name, possition, employee_avalibility)
-VALUES (2, "Rapheal Sadiq", "Producer", 2)
+VALUES (2, "Rapheal Sadiq", "Producer", 2);
 
 INSERT INTO Equipment (equipment_id, equipment_name, description, studio_id)
-VALUES (1, "Universal Audio 610", "Anolog Console", 1)
+VALUES (1, "Universal Audio 610", "Anolog Console", 1);
 
 INSERT INTO Equipment (equipment_id, equipment_name, description, studio_id)
-VALUES (2, "Sure Dynamic Mic", "Vocal Microphone", 1)
+VALUES (2, "Sure Dynamic Mic", "Vocal Microphone", 1);
 
 INSERT INTO Equipment (equipment_id, equipment_name, description, studio_id)
 VALUES (3, "63 Vox AC30", "Guitar Amplifier", 1)
